4fdfc9c5d24722a6427ba109d50ec5186ded00ab
Jhonny Bro <103278130+JhonnyB0098@users.noreply.github.com>
2023-10-19 16:11:54 +0000

    created sign in api route

--- api/index.js
+++ api/index.js
@@ -8,26 +8,26 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB');
-}).catch((err) => {console.log(err)});
+}).catch((err) => { console.log(err) });
 
 const app = express();
 
 app.use(express.json());
 
 app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    }
+    console.log('Server is running on port 3000');
+}
 );
 
-app.use("/api/user",userRouter);
+app.use("/api/user", userRouter);
 app.use('/api/auth', authRouter);
 
-app.use((err,req,res,next) =>{
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
-    const message= err.message || 'Internal Server Error';
+    const message = err.message || 'Internal Server Error';
 
     return res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
         message,
     });
